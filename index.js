8d3484ce713084b2075eb41d9228a508e83c73d2
turku <turkucakmak@hotmail.com>
2023-05-05 18:22:59 +0300

    Completed Assignmnent

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!";
+  }
+  else if(ride>400 && ride<2000){
+   return "That will be twenty bucks.";
+
+  }
+
+  else if (ride>=2000 && ride<=2500){
+    return "I will gladly take your thirty bucks.";
+  }
+
+  else if (ride>2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city=="NYC"){
+    return "Ok, sounds good.";
+  }
+  else{
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ if (tip=="generous"){
+  return "Thank you so much.";
+ }
+ else if (tip=="not as generous"){
+  return "Thank you.";
+ }
+ else {
+  return "Bye.";
+ }
 }
+ 
+  scuberGreetingForFeet(199);
+  scuberGreetingForFeet(2001);
+  scuberGreetingForFeet(2501);
+  ternaryCheckCity('NYC');
+  ternaryCheckCity('Pittsburgh');
+  switchOnCharmFromTip('generous');
+  switchOnCharmFromTip('not as generous');
+  switchOnCharmFromTip('thanks for everything');
